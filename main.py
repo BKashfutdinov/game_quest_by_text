f176a3d1665fbf59c6fe31883607da54a2c23e5c
SueRTe31 <agf2001@gmail.com>
2017-09-22 22:45:55 +0300

    first functions

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import math
 import os
 
-print("123")
+health = 100
+agility = 0
+strength = 0
+
+
+def hit(agility, strength):
+    pass
+
+
+def fight():
+    health_of_zombie = 50
+    number_of_zombie = 1
+    hit()
+
+
+fight()
