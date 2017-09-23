49f08ff6d21d71cba719d00a17b24d6773ce333d
SueRTe31 <agf2001@gmail.com>
2017-09-23 12:03:50 +0300

    hit definition

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import math
 import os
+import random
 
 health = 100
-agility = 0
-strength = 0
+agility = 50
+strength = 10
 
 
-def hit(agility, strength):
-    pass
+def hit(health, agility, strength):
+    h = health - int(random.randint(0, 10) * (agility / 100 + 1)) - strength
+    # print(h)
+    return h
 
 
 def fight():
     health_of_zombie = 50
     number_of_zombie = 1
-    hit()
+    while health_of_zombie > 0:
+        health_of_zombie = hit(health_of_zombie, agility, strength)
+        print(health_of_zombie)
 
 
 fight()
