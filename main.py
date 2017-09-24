488340b827fcc22f5a2e7e15c0b1ca785fb88316
BKashfutdinov <agf2001@gmail.com>
2017-09-24 23:18:47 +0300

    add locations

--- main.py
+++ main.py
@@ -21,4 +21,23 @@ def fight():
         print(health_of_zombie)
 
 
-fight()
+locations = ["Вы находитесь в пещере",
+             "Вы находитесь на подводной лодке",
+             "Вы находитесь в комнате без окон",
+             "Вы находитесь в комнате с окнами",
+             "Вы находитесь в поле",
+             "Вы находитесь в лесу"]
+
+loc = locations[random.randint(0, len(locations) - 1)]
+action=1
+while action!=0:
+    print(loc)
+    action=int(input("Что вы хотите сделать: 0-закончить; 1-начать драку; 2-сбежать в другую локацию"))
+    print(action)
+
+    if action==1:
+        fight()
+    elif action==2:
+        loc=locations[random.randint(0, len(locations) - 1)]
+    elif action==0:
+        exit(0)
