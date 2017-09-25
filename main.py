99812ffb65ec1cd75774b2a336fc14d94ffa1d35
BKashfutdinov <agf2001@gmail.com>
2017-09-25 22:53:07 +0300

    add escape function

--- main.py
+++ main.py
@@ -16,21 +16,22 @@ def hit(health, agility, strength):
 def fight():
     health_of_zombie = 50
     number_of_zombie = 1
-    while health_of_zombie > 0:
+    do = {'1':hit, '2': defence, '3': escape}
 
+    while health_of_zombie > 0:
 
         try:
             action_fight = int(input("1-ударить, 2-оборонятся, 3-попытаться убежать из боя"))
         except ValueError:
             action_fight = 3
 
-
         if action_fight == 1:
             health_of_zombie = hit(health_of_zombie, agility, strength)
         elif action_fight == 2:
             print("Попытка защититься")
         else:
-            print("Вы пытаетесь убежать, break")
+            print("Вы пытаетесь убежать")
+            escape()
 
         if health_of_zombie <= 0:
             print("Вы убили зомби")
@@ -41,6 +42,9 @@ def fight():
 def defence(a):
     defence_point = a // 5
     return defence_point
+
+def escape():
+    pass
 
 
 locations = ["Вы находитесь в пещере",
@@ -64,5 +68,5 @@ while action != 0:
         fight()
     elif action == 2:
         loc = locations[random.randint(0, len(locations) - 1)]
-    #elif action == 0:
-    #    exit(0)
+        # elif action == 0:
+        #    exit(0)
